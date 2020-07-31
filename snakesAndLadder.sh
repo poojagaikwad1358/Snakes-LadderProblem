4f31e25dca56e69db0a1198bf4d802eee66f321e
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-07-31 21:47:20 +0530

    Added: playing function for no play,ladder,snake

--- snakesAndLadder.sh
+++ snakesAndLadder.sh
@@ -3,10 +3,36 @@
 #constants
 START_POS=0;
 PLAYERS=1;
+NO_PLAY=1;
+LADDER=2;
+SNAKE=3;
 
+#variables
+position=$START_POS
+
+#rolling dice to get number between 1 to 6
 function diceRoll()
 {
 	random=$(( (RANDOM%6)+1 ))
 	echo "Dice roll result: "$random
 }
-diceRoll
+
+#Function to get no play,ladder,snake
+function playing(){
+	choice=$(($((RANDOM%3))+1))
+	diceRoll
+	case $choice in
+		$NO_PLAY)
+			echo "No move player have same position"
+			;;
+		$LADDER)
+			position=$(($position+$random))
+			echo "got Ladder move forword by $position"
+			;;
+		$SNAKE)
+			position=$(($position-$random))
+			echo "got Snake move backward by $position"
+			;;
+	esac
+}
+playing
