3274f8895879f8af8433767a6b84bcbc4b16571a
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-08-02 21:32:45 +0530

    Added: Playing for 2 players & display winner .

--- snakesAndLadder.sh
+++ snakesAndLadder.sh
@@ -1,15 +1,22 @@
 #!/bin/bash -x
 
+#dictionary array
+declare -a players
+
 #Constants
 START_POS=0;
-PLAYERS=1;
 NO_PLAY=1;
 LADDER=2;
 SNAKE=3;
 
 #Variables
-position=$START_POS
 noOfDiceRolls=0
+noOfPlayers=2
+
+for(( i=1; i<=$noOfPlayers; i++ ))
+do
+	players[$i]=0;
+done
 
 #Rolling dice to get number between 1 to 6.
 function diceRoll()
@@ -33,30 +40,54 @@ function playing()
 			;;
 		$LADDER)
 			#Performing operation to get exact winning position 100.
-			temp=$(( $position + $random ))
+			temp=$(( ${players[key]} + $random ))
 			if [ $temp -gt 100 ]
 			then
 				echo "You can't move."
 			else
-				position=$temp
-				echo "You got Ladder move forword by $position."
+				players[$key]=$temp
+				echo "You got Ladder move forword by ${players[$key]}."
+				if [ ${players[$key]} -ne 100 ]
+				then
+					playing
+				fi
 			fi
 			;;
 		$SNAKE)
-			position=$(( $position - $random ))
-			if [ $position -lt 0 ]
+			players[$key]=$(( ${players[$key]} - $random ))
+			if [ ${players[$key]} -lt 0 ]
 			then
-				position=$START_POS
+				players[$key]=$START_POS
 			fi
-			echo "You got Snake move backward by $position."
+			echo "You got Snake move backward by ${players[$key]}."
 			;;
 	esac
 }
 
-#Performing continuous playing to reach at 100 position.
-while [ $position -lt 100 ]
+#variables
+flag=0
+
+#Performing operation to display turn & winner.
+while [ $flag -ne 1 ]
 do
-	playing
+	for key in ${!players[@]}
+	do
+		echo "Player $key turn"
+		playing
+		if [ ${players[$key]} -eq 100 ]
+		then
+			flag=1
+			echo "Player $key wins."
+			break;
+		fi
+	done
 done
 
 echo "Total number of times dice rolls: "$noOfDiceRolls
+
+#Displaying players current position.
+echo "Current positions of players are: "
+for key in ${!players[@]}
+do
+	echo "Players $key": ${players[$key]}
+done
